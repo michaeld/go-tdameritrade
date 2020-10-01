8138dca720f8368ea183c467274c8fc3cbd43da3
Michael Dumont <mrdumont@amazon.com>
2020-10-01 15:41:44 -0500

    Replace float64 with custom JSONFloat to support JSON Unmarshaling

--- tdameritrade/chains.go
+++ tdameritrade/chains.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"strconv"
 )
 
 // ChainsService handles communication with the chains related methods of
@@ -12,6 +13,21 @@ import (
 // TDAmeritrade API docs: https://developer.tdameritrade.com/option-chains/apis
 type ChainsService struct {
 	client *Client
+}
+
+type JSONFloat float64
+
+func (i *JSONFloat) UnmarshalJSON(data []byte) error {
+	f, err := strconv.ParseFloat(string(data), 64)
+
+	if err != nil {
+		*i = 0
+		return nil
+	}
+
+	*i = JSONFloat(f)
+
+	return nil
 }
 
 type Underlying struct {
@@ -41,53 +57,53 @@ type Underlying struct {
 }
 
 type ExpDateOption struct {
-	PutCall                string  `json:"putCall"`
-	Symbol                 string  `json:"symbol"`
-	Description            string  `json:"description"`
-	ExchangeName           string  `json:"exchangeName"`
-	Bid                    float64 `json:"bid"`
-	Ask                    float64 `json:"ask"`
-	Last                   float64 `json:"last"`
-	Mark                   float64 `json:"mark"`
-	BidSize                int     `json:"bidSize"`
-	AskSize                int     `json:"askSize"`
-	BidAskSize             string  `json:"bidAskSize"`
-	LastSize               float64 `json:"lastSize"`
-	HighPrice              float64 `json:"highPrice"`
-	LowPrice               float64 `json:"lowPrice"`
-	OpenPrice              float64 `json:"openPrice"`
-	ClosePrice             float64 `json:"closePrice"`
-	TotalVolume            int     `json:"totalVolume"`
-	TradeDate              string  `json:"tradeDate"`
-	TradeTimeInLong        int     `json:"tradeTimeInLong"`
-	QuoteTimeInLong        int     `json:"quoteTimeInLong"`
-	NetChange              float64 `json:"netChange"`
-	Volatility             float64 `json:"volatility"`
-	Delta                  float64 `json:"delta"`
-	Gamma                  float64 `json:"gamma"`
-	Theta                  float64 `json:"theta"`
-	Vega                   float64 `json:"vega"`
-	Rho                    float64 `json:"rho"`
-	OpenInterest           int     `json:"openInterest"`
-	TimeValue              float64 `json:"timeValue"`
-	TheoreticalOptionValue float64 `json:"theoreticalOptionValue"`
-	TheoreticalVolatility  float64 `json:"theoreticalVolatility"`
-	OptionDeliverablesList string  `json:"optionDeliverablesList"`
-	StrikePrice            float64 `json:"strikePrice"`
-	ExpirationDate         int     `json:"expirationDate"`
-	DaysToExpiration       int     `json:"daysToExpiration"`
-	ExpirationType         string  `json:"expirationType"`
-	LastTradingDate        int     `json:"lastTradingDay"`
-	Multiplier             float64 `json:"multiplier"`
-	SettlementType         string  `json:"settlementType"`
-	DeliverableNote        string  `json:"deliverableNote"`
-	IsIndexOption          bool    `json:"isIndexOption"`
-	PercentChange          float64 `json:"percentChange"`
-	MarkChange             float64 `json:"markChange"`
-	MarkPercentChange      float64 `json:"markPercentChange"`
-	InTheMoney             bool    `json:"inTheMoney"`
-	Mini                   bool    `json:"mini"`
-	NonStandard            bool    `json:"nonStandard"`
+	PutCall                string    `json:"putCall"`
+	Symbol                 string    `json:"symbol"`
+	Description            string    `json:"description"`
+	ExchangeName           string    `json:"exchangeName"`
+	Bid                    float64   `json:"bid"`
+	Ask                    float64   `json:"ask"`
+	Last                   float64   `json:"last"`
+	Mark                   float64   `json:"mark"`
+	BidSize                int       `json:"bidSize"`
+	AskSize                int       `json:"askSize"`
+	BidAskSize             string    `json:"bidAskSize"`
+	LastSize               float64   `json:"lastSize"`
+	HighPrice              float64   `json:"highPrice"`
+	LowPrice               float64   `json:"lowPrice"`
+	OpenPrice              float64   `json:"openPrice"`
+	ClosePrice             float64   `json:"closePrice"`
+	TotalVolume            int       `json:"totalVolume"`
+	TradeDate              string    `json:"tradeDate"`
+	TradeTimeInLong        int       `json:"tradeTimeInLong"`
+	QuoteTimeInLong        int       `json:"quoteTimeInLong"`
+	NetChange              float64   `json:"netChange"`
+	Volatility             JSONFloat `json:"volatility"`
+	Delta                  JSONFloat `json:"delta"`
+	Gamma                  JSONFloat `json:"gamma"`
+	Theta                  JSONFloat `json:"theta"`
+	Vega                   JSONFloat `json:"vega"`
+	Rho                    JSONFloat `json:"rho"`
+	OpenInterest           int       `json:"openInterest"`
+	TimeValue              float64   `json:"timeValue"`
+	TheoreticalOptionValue JSONFloat `json:"theoreticalOptionValue"`
+	TheoreticalVolatility  JSONFloat `json:"theoreticalVolatility"`
+	OptionDeliverablesList string    `json:"optionDeliverablesList"`
+	StrikePrice            float64   `json:"strikePrice"`
+	ExpirationDate         int       `json:"expirationDate"`
+	DaysToExpiration       int       `json:"daysToExpiration"`
+	ExpirationType         string    `json:"expirationType"`
+	LastTradingDate        int       `json:"lastTradingDay"`
+	Multiplier             float64   `json:"multiplier"`
+	SettlementType         string    `json:"settlementType"`
+	DeliverableNote        string    `json:"deliverableNote"`
+	IsIndexOption          bool      `json:"isIndexOption"`
+	PercentChange          float64   `json:"percentChange"`
+	MarkChange             float64   `json:"markChange"`
+	MarkPercentChange      float64   `json:"markPercentChange"`
+	InTheMoney             bool      `json:"inTheMoney"`
+	Mini                   bool      `json:"mini"`
+	NonStandard            bool      `json:"nonStandard"`
 }
 
 // the first string is the exp date.  the second string is the strike price.
